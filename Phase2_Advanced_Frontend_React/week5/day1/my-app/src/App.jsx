f4cce0a8dabe5629fd71e6b7e9216aac0d90acf5
SudheerKumarDas <samratsudheerdas@gmail.com>
2025-12-02 11:40:30 +0545

    msg sent

---
+++ Phase2_Advanced_Frontend_React/week5/day1/my-app/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Hello, Sudheer!</h1>
+        <p>Welcome to my React app</p>
+        <h2 className="title">Welcome!</h2>
+        <p>Your score is {10 + 20}</p>
+      </div>
+    </>
+  )
+}
+
+export default App
