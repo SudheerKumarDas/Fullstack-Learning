67db246b67bfe740219c45e6c448e647a371c5bd
SudheerKumarDas <samratsudheerdas@gmail.com>
2025-12-03 22:46:22 +0545

    day 1 completed

---
+++ TypeScript/day1/script.js
@@ -0,0 +1,6 @@
+function greet(name){
+    return "Hello, "+ name;
+}
+
+console.log(greet("Sudheer"));
+console.log(greet(42));
---
+++ TypeScript/day1/script1.ts
@@ -0,0 +1,6 @@
+function greet1(name : string):string {
+    return `Hello, ${name}`;
+}
+
+console.log(greet1("Sudheer"));
+console.log(greet1(43));//it is compiled but it shows error 
