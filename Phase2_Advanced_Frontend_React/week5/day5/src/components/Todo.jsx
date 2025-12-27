ff2742f6aedbac71db32057b2bbe5c8a5ce1d45f
SudheerKumarDas <samratsudheerdas@gmail.com>
2025-12-27 23:23:51 +0545

    useEffect api call and loading screeen

--- Phase2_Advanced_Frontend_React/week5/day5/src/components/Todo.jsx
+++ Phase2_Advanced_Frontend_React/week5/day5/src/components/Todo.jsx
@@ -1,17 +1,26 @@
 import { useEffect, useState } from "react"
 
 function Todo(){
-    const [todo, setTodo]=useState("todo1");
+    const [todo, setTodo]=useState(1);
+    const [data, setData] = useState({});
+    const [loading, setLoading] = useState(true);
 
     useEffect(()=>{
-        fetch('hhtps:www.example.com/todo/');
-    })
+        fetch('https://jsonplaceholder.typicode.com/todos/'+todo)
+            .then(async res => {
+                const json = await res.json();
+                setData(json);
+                setLoading(false);
+            })
+    },[todo]);
     return (
         <div>
-            <button onClick={()=>setTodo("todo1")} style={{color: todo == "todo1" ? "red" : "black"}}>Todo1</button>
-            <button onClick={()=>setTodo("todo2")} style={{color: todo == "todo2" ? "red" : "black"}}>Todo2</button>
-            <button onClick={()=>setTodo("todo3")} style={{color: todo == "todo3" ? "red" : "black"}}>Todo3</button>
-            <button onClick={()=>setTodo("todo4")} style={{color: todo == "todo4" ? "red" : "black"}}>Todo4</button>
+            <button onClick={()=>setTodo(1)} style={{color: todo == 1 ? "red" : "black"}}>Todo1</button>
+            <button onClick={()=>setTodo(2)} style={{color: todo == 2 ? "red" : "black"}}>Todo2</button>
+            <button onClick={()=>setTodo(3)} style={{color: todo == 3 ? "red" : "black"}}>Todo3</button>
+            <button onClick={()=>setTodo(4)} style={{color: todo == 4 ? "red" : "black"}}>Todo4</button>
+            <br />
+            {loading ? "Loading..." : data.title}
         </div>
     )
 }
