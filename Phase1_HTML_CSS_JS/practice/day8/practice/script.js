819eb7e219edf5a36857a8a04e474086ce1dc993
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-01-14 18:34:45 +0545

    js small projects

--- Phase1_HTML_CSS_JS/practice/day8/practice/script.js
+++ Phase1_HTML_CSS_JS/practice/day8/practice/script.js
@@ -53,3 +53,57 @@
 //     document.body.style.color = "black";
 // })
 
+//  Toggle Text
+// const message = document.querySelector("#message");
+// const toggleText = document.querySelector("#toggleText");
+
+// toggleText.addEventListener("click", function(){
+//     let textMessage = message.innerText;
+//     let newMessage = textMessage==="Hello" ? "Goodbye" : "Hello";
+//     message.innerText=newMessage; 
+// })
+
+// Disable button after submit
+// const submitBtn = document.querySelector("#submit");
+// const statusIn = document.querySelector("#status");
+
+// submitBtn.addEventListener("click",function(){
+//     statusIn.innerText = "Submitted";
+//     submitBtn.disabled = true;
+//     submitBtn.innerText = "Submitted"
+// })
+
+//  EXERCISE 6: Simple Password Reveal
+//  Toggle password visibility
+// const password = document.querySelector("#password");
+// const showBtn = document.querySelector("#show");
+
+// showBtn.addEventListener("click", function(){
+//     if(password.type === "password"){
+//         password.type = "text";
+//         showBtn.innerText = "Hide";
+//     }
+//     else{
+//         password.type = "password";
+//         showBtn.innerText="Show";
+//     }    
+// })
+
+// EXERCISE 7: Character Counter (Challenging)
+// HTML
+// <input type="text" id="name" />
+// <p id="count">0 characters</p>
+
+// Tasks 🎯
+// Update character count while typing
+// Show: X characters
+// 📌 Event:
+// input.addEventListener("input", ...)
+
+const input = document.querySelector("#name");
+const count = document.querySelector("#count");
+
+input.addEventListener("input",function(){
+        let characters = input.value.length;
+        count.innerText = characters + " characters";
+})
