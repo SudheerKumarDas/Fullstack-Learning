179ac896458272abee3b691c6a3ab55cd6511bd6
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-05-08 13:44:56 +0545

    useEffect hooks real usecases i.e. calling APIs

--- checkpoints/checkpoint1/src/components/hooks/UseEffectHook.jsx
+++ checkpoints/checkpoint1/src/components/hooks/UseEffectHook.jsx
@@ -1,22 +1,63 @@
 import { useEffect } from "react"
 import { useState } from "react";
 
+// export default function UseEffectHook(){
+//     const [count, setCount]=useState(0);
+//     console.log("component rendered");
+//     useEffect(()=>{
+//         console.log("useEffect hook ran");
+//     })
+//     useEffect(()=>{
+//         console.log("this useEffect ran only once when mounted first");
+//     },[]);
+//     useEffect(()=>{
+//         console.log("this useEffect ran when its dependency array i.e. count changes !!");
+//     },[count]);
+//     return (
+//         <>
+//             <h1>Hello useEffect</h1>
+//             <button onClick={()=>setCount(count+1)}>Counter increase button {count}</button>
+//         </>
+//     )
+// }
+
+
+// export default function UseEffectHook(){
+//      const [users,setUsers]=useState([]);
+
+//      useEffect(()=>{
+//             fetch('https://jsonplaceholder.typicode.com/users')
+//                 .then(res => res.json())
+//                     .then(data => setUsers(data));
+//      },[])
+//     return (
+//         <>
+//             <h1>List of users</h1>
+//             {
+//                 users.map(user => {
+//                     return <li key={user.id}>{user.name}</li>
+//                 })
+//             }
+//         </>
+//     )
+// }
+
 export default function UseEffectHook(){
-    const [count, setCount]=useState(0);
-    console.log("component rendered");
+    const [users,setUsers]=useState([]);
+    const [userId, setUsersId]=useState(1);
+    
+    console.log("components renders");
+
     useEffect(()=>{
-        console.log("useEffect hook ran");
-    })
-    useEffect(()=>{
-        console.log("this useEffect ran only once when mounted first");
-    },[]);
-    useEffect(()=>{
-        console.log("this useEffect ran when its dependency array i.e. count changes !!");
-    },[count]);
+            fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+                .then(res => res.json())
+                    .then(data => setUsers(data))
+    },[userId])
     return (
-        <>
-            <h1>Hello useEffect</h1>
-            <button onClick={()=>setCount(count+1)}>Counter increase button {count}</button>
-        </>
+        <div>
+            <h1>User : {userId}</h1>
+            <h2>{users.name}</h2>
+            <button onClick={()=>setUsersId(userId+1)}>Change User</button>
+        </div>
     )
 }
