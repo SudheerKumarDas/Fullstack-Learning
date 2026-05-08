a556e64904dce42142fbe464bb8acb9a1dc6c9ed
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-05-08 13:16:45 +0545

    useEffect hook learned about dependcies array and side effects

--- checkpoints/checkpoint1/src/components/hooks/UseEffectHook.jsx
+++ checkpoints/checkpoint1/src/components/hooks/UseEffectHook.jsx
@@ -1,12 +1,22 @@
 import { useEffect } from "react"
+import { useState } from "react";
 
 export default function UseEffectHook(){
+    const [count, setCount]=useState(0);
+    console.log("component rendered");
     useEffect(()=>{
-        console.log("this is rendered on every re-render");
+        console.log("useEffect hook ran");
     })
+    useEffect(()=>{
+        console.log("this useEffect ran only once when mounted first");
+    },[]);
+    useEffect(()=>{
+        console.log("this useEffect ran when its dependency array i.e. count changes !!");
+    },[count]);
     return (
         <>
             <h1>Hello useEffect</h1>
+            <button onClick={()=>setCount(count+1)}>Counter increase button {count}</button>
         </>
     )
 }
