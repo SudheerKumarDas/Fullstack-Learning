6b4d16dcde5a467cc7d3477aa65fc8d5c795d041
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-05-10 15:21:53 +0545

    map,reduce,find,filter all done

--- Phase1_HTML_CSS_JS/practice/day21/test1.js
+++ Phase1_HTML_CSS_JS/practice/day21/test1.js
@@ -78,8 +78,8 @@ console.log(linuxDistros[3]);
 console.log(linuxDistros[4]);// now it gives value because we push to pos 5 in array
 
 const profile = {
-    name : "sudheer",
-    age : 25
+    names : "sudheer",
+    ages : 25
 }
 console.log(profile);
 console.log(typeof(profile));
@@ -90,3 +90,98 @@ console.log(profile);
 
 console.log(profile.name);
 
+// destructuring 
+// extracting values easily from arrays or objects
+
+const [linux1,linux2]=linuxDistros;
+console.log(linux1,linux2);
+
+const {names, ages}=profile;
+console.log(names,ages);
+
+//template literals
+
+let linux = "arch"
+console.log(`Hello , ${linux}`);
+
+let lines = `
+    this is line 1
+    this is line 2
+    this is line 3
+`
+console.log(lines);
+
+// spread operator
+// copy or expands values
+
+const arr1 = [1,2,3];
+const arr2 = [...arr1, 4, 5];
+console.log(arr2);
+
+//objects spread operator on 
+const user = {
+    name: "sudheer"
+}
+const updatedUser = {
+    ...user,
+    age: 25
+};
+console.log(updatedUser);
+
+// rest operator
+//Collects multiple values into one.
+const [first, ...second]=[1,2,3,4,5,6];
+console.log(first, second);
+
+//Loops and array methods
+for(let i=0;i<5;i++){
+    console.log(i);
+}
+
+const nums=[1,2,3,4,5]
+
+for (const num of nums) {
+    console.log(num);
+}
+
+//array methods
+// map
+const oddNumbers = [1,3,5,7,9,11];
+const doubledOddNumbers = oddNumbers.map((num)=> num*2);
+console.log(oddNumbers);
+console.log(doubledOddNumbers);
+
+const kvArray = [
+    {key:1, value:10},
+    {key:2, value:20},
+    {key:3, value:30}
+];
+
+const reFormattedArray = kvArray.map(({key,value})=>({[key]:value}));
+console.log(reFormattedArray);
+
+
+const users = ["Ram","Hari"];
+const result = users.map((user)=>`hello,${user}`);
+console.log(result);
+
+//filter
+const numbers = [1,2,3,4,5,6];
+const evenNums = numbers.filter((num)=>num%2===0);
+console.log(evenNums);
+
+//reduce method
+const sumOfNums = numbers.reduce((acc,curr)=>{
+    return acc+curr;
+},0);
+console.log(sumOfNums);
+
+//find
+const newUsers = [
+    {id:1,name:"sudheer"},
+    {id:2,name:"samrat"},
+    {id:2,name:"sunidhi"}
+];
+
+const userFound = newUsers.find((user)=>user.id===2);//search and finds the first matched user id 
+console.log(userFound);
