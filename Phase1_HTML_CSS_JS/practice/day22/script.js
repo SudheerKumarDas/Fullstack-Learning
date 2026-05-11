6c14dce513e82894d0e63d01d21b54562974920f
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-05-11 14:38:00 +0545

    learned about DOM and DOM manupulation

---
+++ Phase1_HTML_CSS_JS/practice/day22/script.js
@@ -0,0 +1,12 @@
+// accessing DOM elements
+
+const h1 = document.getElementById("heading");
+const btn = document.getElementById("btn");
+
+// DOM manipulation
+h1.innerText = "New Heading..."
+h1.innerHTML = "<span>Hello</span>"
+
+//changing styles of html
+h1.style.color="blue";
+btn.style.background="pink";
