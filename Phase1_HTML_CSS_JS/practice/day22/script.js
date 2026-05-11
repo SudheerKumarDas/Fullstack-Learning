170f840815fd51080b6c23dac2d06115d2023f0f
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-05-11 15:11:55 +0545

    dom manipulation

--- Phase1_HTML_CSS_JS/practice/day22/script.js
+++ Phase1_HTML_CSS_JS/practice/day22/script.js
@@ -10,3 +10,12 @@ h1.innerHTML = "<span>Hello</span>"
 //changing styles of html
 h1.style.color="blue";
 btn.style.background="pink";
+btn.style.padding="10px 20px";
+btn.style.fontWeight="700";
+
+//creating a new element in js using DOM
+const p = document.createElement("p");
+p.textContent = "this is a new paragraph.";
+
+// we also need to append the new created element to html page so
+document.body.appendChild(p);
