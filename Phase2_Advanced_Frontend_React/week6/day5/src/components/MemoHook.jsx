ff651298c822c4ae5072d04c45603068424a17f2
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-05-14 23:42:50 +0545

    useMemo hook learned

---
+++ Phase2_Advanced_Frontend_React/week6/day5/src/components/MemoHook.jsx
@@ -0,0 +1,33 @@
+import { useMemo, useState } from "react";
+
+function slowFunction(num) {
+  console.log("Calculating...");
+
+  for (let i = 0; i < 1000000000; i++) {}
+
+  return num * 2;
+}
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
+  const result = useMemo(() => {
+    return slowFunction(count);
+  }, [count]);
+
+  return (
+    <>
+      <h1>{result}</h1>
+
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+
+      <input
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+    </>
+  );
+}
