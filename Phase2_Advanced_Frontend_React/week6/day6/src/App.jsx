3b9eb7f6939c751ff041b984ea95795b50a2afba
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-05-19 00:58:00 +0545

    learned about useReducer hook

--- Phase2_Advanced_Frontend_React/week6/day6/src/App.jsx
+++ Phase2_Advanced_Frontend_React/week6/day6/src/App.jsx
@@ -1,13 +1,39 @@
 //import { useState } from 'react'
-
+// const [state, dispatch] = useReducer(reducer, initialState)
+import { useReducer } from 'react'
 import './App.css'
 
+const initialState = { count: 0}
+
+function reducer(state, action){
+ switch(action.type){
+  case "increment" : 
+    return {count : state.count + 1};
+
+  case "decrement" :
+    return {count : state.count - 1};
+
+  case "reset" :
+    return { count : 0 };
+
+  default :
+     return state;
+ }
+
+}
+
 function App() {
-  
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <>
-      <h1>Hello</h1>
+      <h1>{state.count}</h1>
+
+      <button onClick={()=>dispatch({type:"increment"})}>+</button>
+
+      <button onClick={()=>dispatch({type:"decrement"})}>-</button>
+
+      <button onClick={()=>dispatch({type:"reset"})}>reset</button>
     </>
   )
 }
