0b13b44523475928f5e9e7bd3e819e77056e70f8
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-05-19 12:38:07 +0545

    learned about react routing : BrowserRouter, Routes, Route, Link in react-router-dom

--- Phase2_Advanced_Frontend_React/week6/day7/src/App.jsx
+++ Phase2_Advanced_Frontend_React/week6/day7/src/App.jsx
@@ -1,15 +1,35 @@
-import { useState } from 'react'
-
 import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 function App() {
   
 
   return (
     <>
-      <h1>Hello</h1>
+      <BrowserRouter>
+        <Link to="/">Allen</Link> |
+        <Link to="/neet-program-class11">Class 11</Link> |
+        <Link to="/neet-program-class12">Class 12</Link>
+          <Routes>
+              <Route path='/neet-program-class11' element={<ProgramClass11/>}></Route>
+              <Route path='/neet-program-class12' element={<ProgramClass12/>}></Route>
+              <Route path='/' element={<Landing/>}></Route>  
+          </Routes>      
+      </BrowserRouter>
     </>
   )
+}
+
+function ProgramClass11(){
+  return <h1>Class 11</h1>
+}
+
+function ProgramClass12(){
+  return <h1>Class 12</h1>
+}
+
+function Landing(){
+  return <h1>Landing</h1>
 }
 
 export default App
