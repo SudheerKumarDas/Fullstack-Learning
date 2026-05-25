ab32b2d9542438912c123c453396a0df3854a6bc
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-05-25 21:24:30 +0545

    getAllNotes controller sorted by latest created notes

--- Phase3_Backend_Fundamentals/week9/day7/src/controllers/notesControllers.js
+++ Phase3_Backend_Fundamentals/week9/day7/src/controllers/notesControllers.js
@@ -3,7 +3,7 @@ import Note from "../models/Note.js"
 
 export const getAllNotes = async (req,res) => {
     try {
-            const notes = await Note.find();
+            const notes = await Note.find().sort({createdAt:-1});//sort by latest created notes
             res.status(200).json(notes);
         
     } catch (error) {
