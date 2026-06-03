e4dc0e11876c25889107bc4f7cb7c33460416aea
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-06-03 13:10:00 +0545

    user created with name,email and password and also save the data to the database

--- Phase3_Backend_Fundamentals/week11/day1/server.js
+++ Phase3_Backend_Fundamentals/week11/day1/server.js
@@ -1,20 +1,50 @@
 import express from "express";
 import dotenv from "dotenv"
+import { connectDB } from "./config/db.js";
+import User from "./models/User.js"
 
 dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
 
 app.post("/api/auth/register", async (req,res) => {
         try {
             const {name, email, password} = req.body;
+
+            const existingUser = await User.findOne({email});
+
+            if(existingUser){
+                return res.status(400).json({
+                    message:"User already exists"
+                })
+            }
+
+            const user = await User.create(
+                {
+                    name,
+                    email,
+                    password
+                }
+            )
+            res.status(201).json(
+                {
+                    message:"User created successfully"
+                }
+            )
         } catch (error) {
-            
+            console.log("Error registering user",error);
+            res.status(500).json({
+                message:"Internal server error"
+            })
         }
 })
 
-app.listen(5000,()=>{
-    console.log("Server is listening to the port:5000")
+connectDB();
+
+app.listen(3000,()=>{
+    console.log("Server is listening to the port:3000")
 })
