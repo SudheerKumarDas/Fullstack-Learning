9fce957ecd09e9c5bcb37e838ff7fdf78881bcd0
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-06-03 13:39:18 +0545

    password hashed using bcrypt

--- Phase3_Backend_Fundamentals/week11/day1/server.js
+++ Phase3_Backend_Fundamentals/week11/day1/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv"
 import { connectDB } from "./config/db.js";
-import User from "./models/User.js"
+
+import authRoutes from "./Routes/authRoutes.js"
 
 dotenv.config();
 
@@ -9,39 +10,11 @@ const app = express();
 
 app.use(express.json());
 
+app.use("/api/auth",authRoutes);
+
 const PORT = process.env.PORT || 3000;
 
-app.post("/api/auth/register", async (req,res) => {
-        try {
-            const {name, email, password} = req.body;
 
-            const existingUser = await User.findOne({email});
-
-            if(existingUser){
-                return res.status(400).json({
-                    message:"User already exists"
-                })
-            }
-
-            const user = await User.create(
-                {
-                    name,
-                    email,
-                    password
-                }
-            )
-            res.status(201).json(
-                {
-                    message:"User created successfully"
-                }
-            )
-        } catch (error) {
-            console.log("Error registering user",error);
-            res.status(500).json({
-                message:"Internal server error"
-            })
-        }
-})
 
 connectDB();
 
