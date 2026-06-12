6737f8f56112444dbe39bb3547d908f385a71313
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-06-12 12:46:00 +0545

    db connected

--- Phase3_Backend_Fundamentals/week11/day6/server.js
+++ Phase3_Backend_Fundamentals/week11/day6/server.js
@@ -1,25 +1,28 @@
 import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.post("/signup",()=>{
-
-})
-
-app.post("/signin",()=>{
+app.post("/signup",(req,res)=>{
 
 })
 
-app.post("/todo",()=>{
+app.post("/signin",(req,res)=>{
 
 })
 
-app.get("/todos",()=>{
+app.post("/todo",(req,res)=>{
+
+})
+
+app.get("/todos",(req,res)=>{
 
 })
 
 app.listen(PORT, ()=>{
-
+    console.log("server is listening to the port ", PORT);
 })
