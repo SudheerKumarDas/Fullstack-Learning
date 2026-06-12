bc1914e13e0d5343f09513c6541a004a37baa505
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-06-12 12:37:58 +0545

    init

---
+++ Phase3_Backend_Fundamentals/week11/day6/server.js
@@ -0,0 +1,25 @@
+import express from "express"
+
+const app = express();
+
+const PORT = 3000;
+
+app.post("/signup",()=>{
+
+})
+
+app.post("/signin",()=>{
+
+})
+
+app.post("/todo",()=>{
+
+})
+
+app.get("/todos",()=>{
+
+})
+
+app.listen(PORT, ()=>{
+
+})
