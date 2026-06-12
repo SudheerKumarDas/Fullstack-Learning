5f3db9845b9433d7e5f485c619f7a169bc46b54f
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-06-12 12:50:47 +0545

    user and todo model created

--- Phase3_Backend_Fundamentals/week11/day6/src/models/dbModel.js
+++ Phase3_Backend_Fundamentals/week11/day6/src/models/dbModel.js
@@ -1,1 +1,20 @@
 import mongoose from "mongoose";
+
+const schema = mongoose.Schema;
+const ObjectId = mongoose.ObjectId;
+
+const userSchema = new schema({
+    name:String,
+    email:String,
+    password:String
+})
+
+const todoSchema = new schema({
+    title:String,
+    description:String,
+    userId:ObjectId
+})
+
+export const userModel = mongoose.model("Users",userSchema);
+export const todoModel = mongoose.model("Todos",todoSchema);
+
