29f6fc80a7db6b47ee0870febe014f4548d4c526
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-06-12 13:51:38 +0545

    get the user todos and authenticated

--- Phase3_Backend_Fundamentals/week11/day6/src/models/dbModel.js
+++ Phase3_Backend_Fundamentals/week11/day6/src/models/dbModel.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
+const ObjectId = mongoose.ObjectId;
 
 const userSchema = new Schema({
     name:String,
