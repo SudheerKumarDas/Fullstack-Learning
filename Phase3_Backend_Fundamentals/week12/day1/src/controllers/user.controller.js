3a55d0241c401331d41996cf67d0d34c572c62e9
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-06-18 22:58:46 +0545

    generated access token

--- Phase3_Backend_Fundamentals/week12/day1/src/controllers/user.controller.js
+++ Phase3_Backend_Fundamentals/week12/day1/src/controllers/user.controller.js
@@ -1,79 +1,88 @@
 import userModel from "../models/user.model.js";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-export const register = async (req,res) => {
-    try {
-        const {username, email, password}=req.body;
-        if(!username || !email || !password){
-            return res.status(403).json({
-                message:"provide all the fields"
-            })
-        }
-
-        const alreadyRegistered = await userModel.findOne({
-            $or:[
-                {email},
-                {username}
-            ]
-        })
-        if(alreadyRegistered){
-            return res.status(409).json({
-                message:"username or email already exists"
-            })
-        }
-        const hashedPassword = await bcrypt.hash(password,10);
-        const user = await userModel.create({
-            username,
-            email,
-            password:hashedPassword
-        })
-        res.status(201).json({
-            message:"User registered successfully",
-            user:{
-                username:user.username,
-                email:user.email
-            }
-        })
-    } catch (error) {
-        console.error("Error registering user ",error);
-        res.status(500).json({
-            message:"Internal server failed"
-        })
+export const register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res.status(403).json({
+        message: "provide all the fields",
+      });
     }
-}
 
-export const login = async (req,res) => {
-    try {
-        const {email,password}=req.body;
-        if(!email || !password) {
-            return res.status(409).json({
-                message:"provide all credentials"
-            })
-        }
-        const user = await userModel.findOne({email});
-        if(!user){
-            return res.status(403).json({
-                message:"User not found"
-            })
-        }
+    const alreadyRegistered = await userModel.findOne({
+      $or: [{ email }, { username }],
+    });
+    if (alreadyRegistered) {
+      return res.status(409).json({
+        message: "username or email already exists",
+      });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await userModel.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error("Error registering user ", error);
+    res.status(500).json({
+      message: "Internal server failed",
+    });
+  }
+};
 
-        const isMatchPassword = await bcrypt.compare(password,user.password);
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(409).json({
+        message: "provide all credentials",
+      });
+    }
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(403).json({
+        message: "User not found",
+      });
+    }
 
-        if(!isMatchPassword){
-            return res.status(403).json({
-                message:"credentials not match"
-            })
-        }
+    const isMatchPassword = await bcrypt.compare(password, user.password);
 
-        const refreshToken = await jwt.sign({
+    if (!isMatchPassword) {
+      return res.status(403).json({
+        message: "credentials not match",
+      });
+    }
+
+    const refreshToken = await jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      },
+    );
+
+    const refreshTokenHash = bcrypt.hash(refreshToken,10);
+
+    const accessToken = await jwt.sign(
+        {
             id:user._id
         },
-        process.env.JWT_SECRET
-    ,{
-        
-    }
-)
-    } catch (error) {
-        
-    }
-}
+        process.env.JWT_SECRET,
+        {
+            expiresIn:"15m"
+        }
+    )
+
+  } catch (error) {}
+};
