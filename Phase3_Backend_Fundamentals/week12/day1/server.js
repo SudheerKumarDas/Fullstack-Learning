929af42eff39d71db8612bd2461edbe4ca186a9b
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-06-19 02:17:42 +0545

    tested completely

--- Phase3_Backend_Fundamentals/week12/day1/server.js
+++ Phase3_Backend_Fundamentals/week12/day1/server.js
@@ -1,4 +1,7 @@
 import express from "express";
+import dotenv from "dotenv"
+
+dotenv.config();
 
 import app from "./src/app.js";
 import connectDB from "./src/config/database.js";
--- Phase3_Backend_Fundamentals/week12/day1/src/controllers/user.controller.js
+++ Phase3_Backend_Fundamentals/week12/day1/src/controllers/user.controller.js
@@ -1,9 +1,11 @@
 import userModel from "../models/user.model.js";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"
 
 export const register = async (req, res) => {
   try {
     const { username, email, password } = req.body;
+    console.log(username)
     if (!username || !email || !password) {
       return res.status(403).json({
         message: "provide all the fields",
@@ -24,6 +26,7 @@ export const register = async (req, res) => {
       email,
       password: hashedPassword,
     });
+    console.log(user)
     res.status(201).json({
       message: "User registered successfully",
       user: {
@@ -54,7 +57,11 @@ export const login = async (req, res) => {
       });
     }
 
+    console.log(user)
+
     const isMatchPassword = await bcrypt.compare(password, user.password);
+
+    console.log(isMatchPassword)
 
     if (!isMatchPassword) {
       return res.status(403).json({
@@ -72,38 +79,73 @@ export const login = async (req, res) => {
       },
     );
 
-    const refreshTokenHash = bcrypt.hash(refreshToken,10);
+    console.log(refreshToken)
+
+    const refreshTokenHash = bcrypt.hash(refreshToken, 10);
 
     const accessToken = await jwt.sign(
-        {
-            id:user._id
-        },
-        process.env.JWT_SECRET,
-        {
-            expiresIn:"15m"
-        }
-    )
+      {
+        id: user._id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "15m",
+      },
+    );
 
-    res.cookie("refreshToken",refreshToken,{
-        httpOnly:true,
-        secure:true,
-        sameSite:"strict",
-        maxAge:7*24*60*60*1000
-    })
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
 
     res.status(200).json({
-        message:"logged in successfully",
-        user:{
-            username:user.username,
-            email:user.email
-        },
-        refreshToken
-    })
-
+      message: "logged in successfully",
+      user: {
+        username: user.username,
+        email: user.email,
+      },
+      accessToken,
+    });
   } catch (error) {
-    console.error("Error logging in ",error);
+    console.error("Error logging in ", error);
     res.status(500).json({
-        message:"Internal server error"
-    })
+      message: "Internal server error",
+    });
+  }
+};
+
+export const getMe = async (req, res) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({
+        message: "token not provided",
+      });
+    }
+
+    const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!decoded) {
+      return res.status(401).json({
+        message: "token not valid",
+      });
+    }
+    const user = await userModel.findById(decoded.id);
+
+    res.status(200).json({
+      message: "user fetched successfully",
+      user: {
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error("Error logging in ", error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
   }
 };
