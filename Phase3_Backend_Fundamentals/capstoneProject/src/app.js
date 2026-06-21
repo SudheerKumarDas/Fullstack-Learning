f95013ea70c6951d9579b265ddb2ff055db18aec
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-06-21 11:10:56 +0545

    created user schema

--- Phase3_Backend_Fundamentals/capstoneProject/src/app.js
+++ Phase3_Backend_Fundamentals/capstoneProject/src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 
 import todoRoutes from "./routes/todo.routes.js"
+import userRoutes from "./routes/user.routes.js"
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api",todoRoutes);
+app.use("/api/users/",userRoutes);
 
 export default app;
---
+++ Phase3_Backend_Fundamentals/capstoneProject/src/models/User.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.model({
+    username:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    todos:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Todo"
+    }
+},{
+    timestamps:true
+})
+
+const User = mongoose.model("User",userSchema);
+
+export default User;
