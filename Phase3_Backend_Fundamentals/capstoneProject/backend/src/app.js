22449e2716333348caafd925f4d4b51245863467
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-06-25 14:03:09 +0545

    your message

--- Phase3_Backend_Fundamentals/capstoneProject/backend/src/app.js
+++ Phase3_Backend_Fundamentals/capstoneProject/backend/src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors"
 
 import todoRoutes from "./routes/todo.routes.js"
 import userRoutes from "./routes/user.routes.js"
@@ -6,6 +7,7 @@ import userRoutes from "./routes/user.routes.js"
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/todos",todoRoutes);
 app.use("/users/",userRoutes);
--- Phase3_Backend_Fundamentals/capstoneProject/frontend/src/App.jsx
+++ Phase3_Backend_Fundamentals/capstoneProject/frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import SignUp from './pages/SignUp.jsx'
 
 function App() {
 
   return (
     <>
       <h1>Todo App</h1>
+      <SignUp/>
     </>
   )
 }
