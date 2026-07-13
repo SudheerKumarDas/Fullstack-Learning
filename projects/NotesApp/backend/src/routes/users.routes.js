dc2cb450c9f150dbdce8c5e6ef9db7399b0c5547
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-07-14 02:15:22 +0545

    user login and generate token and token saved in cookie

--- projects/NotesApp/backend/src/routes/users.routes.js
+++ projects/NotesApp/backend/src/routes/users.routes.js
@@ -1,9 +1,10 @@
 import express from "express"
 
-import { userRegister } from "../controllers/users.controllers.js";
+import { userLogin, userRegister } from "../controllers/users.controllers.js";
 
 const router = express.Router();
 
 router.post("/register",userRegister);
+router.post("/login",userLogin);
 
 export default router;
