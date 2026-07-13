d885735bbd859dd4e1d55b48512885a65d08e2c0
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-07-14 00:57:46 +0545

    user model created

---
+++ projects/NotesApp/backend/src/models/users.models.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        trim:true,
+        minLength:3,
+        maxLength:30
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true
+    },
+    password:{
+        type:String,
+        required:true,  
+        minLength:6,
+        trim:true
+    },
+    profileImage:{
+        type:String,
+        default:""
+    }
+},{
+    timestamps:true
+})
+
+const User = mongoose.model("User" , userSchema);
+
+export default User;
