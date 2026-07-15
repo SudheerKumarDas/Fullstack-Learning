a1fca35fdf49dcf039ec226b81021449850ccd3a
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-07-15 13:51:52 +0545

    error handled and refactor the code

--- projects/NotesApp/backend/src/controllers/notes.controllers.js
+++ projects/NotesApp/backend/src/controllers/notes.controllers.js
@@ -1,140 +1,124 @@
 import Note from "../models/notes.models.js";
+import asyncHandler from "../utils/asyncHandler.js";
 
-export const createNotes = async (req, res) => {
-  try {
-    const { id } = req.user;
-    const { title, content } = req.body;
-    if (!title || !content) {
-      return res.status(400).json({
-        message: "Not enough Data",
-      });
-    }
-    const note = await Note.findOne({ title });
-    if (note) {
-      return res.status(409).json({
-        message: "Note already exists",
-      });
-    }
-    const newNote = await Note.create({
-      title,
-      content,
-      user: id,
-    });
-    res.status(201).json({
-      message: "New Note Created",
-      Note: newNote,
-    });
-  } catch (error) {
-    console.error(`Error in creating notes ${error}`);
-    res.status(500).json({
-      message: "Internal server error",
-    });
+export const createNotes = asyncHandler(async (req, res) => {
+  const { id } = req.user;
+  const { title, content } = req.body;
+
+  if (!title || !content) {
+    res.status(400);
+    throw new Error("Not enough Data");
   }
-};
 
-export const getNotes = async (req, res) => {
-  try {
-    const userId = req.user.id;
-    const notes = await Note.find({user:userId});
-    res.status(200).json({
-      message: "Notes fetched successfully",
-      notes: notes,
-    });
-  } catch (error) {
-    console.error(`Error getting notes ${error}`);
-    res.status(500).json({
-      message: "Internal server error",
-    });
+  const note = await Note.findOne({ title, user: id });
+  if (note) {
+    res.status(409);
+    throw new Error("Note already exists");
   }
-};
 
-export const getANotes = async (req, res) => {
-  try {
-    const userId = req.user.id;
-    const id = req.params.id;
-    const note = await Note.findById(id);
-    if (!note) {
-      return res.status(404).json({
-        message: "Note not found",
-      });
-    }
-    if(userId!=note.user.toHexString()){
-      return res.status(403).json({
-        message:"Forbidden"
-      })
-    }
-    res.status(200).json({
-      message: "Note fetched successfully",
-      note: note,
-    });
-  } catch (error) {
-    console.error(`Error in getting a note ${error}`);
-    res.status(500).json({
-      message: "Internal server error",
-    });
+  const newNote = await Note.create({
+    title,
+    content,
+    user: id,
+  });
+
+  res.status(201).json({
+    success: true,
+    message: "New Note Created",
+    data: newNote,
+  });
+});
+
+export const getNotes = asyncHandler(async (req, res) => {
+  const userId = req.user.id;
+  const notes = await Note.find({ user: userId });
+  res.status(200).json({
+    success: true,
+    message: "Notes fetched successfully",
+    data: notes,
+  });
+});
+
+export const getANotes = asyncHandler(async (req, res) => {
+  const userId = req.user.id;
+  const id = req.params.id;
+  const note = await Note.findById(id);
+
+  if (!note) {
+    res.status(404);
+    throw new Error("Note not found");
   }
-};
 
-export const updateNotes = async (req,res) => {
-    try {
-        const userId = req.user.id;
-        console.log(userId);
-        const id = req.params.id;
-        const updateData = req.body;
-        const note = await Note.findById(id);
-        console.log(note.user.toHexString());
-        if(userId!=note.user.toHexString()){
-          return res.status(401).json({
-            message:"Not Authorized"
-          })
-        }
-        const updatedNote = await Note.findByIdAndUpdate(id,updateData,{
-          runValidators:true
-        },{
-            returnDocument:"after"
-        })
-        if(!updatedNote){
-            return res.status(404).json({
-                message:"Note not found"
-            })
-        }
-        res.status(200).json({
-            message:"Note updated successfully",
-            updatedNote:updatedNote
-        })
-    } catch (error) {
-        console.error(`Error in updating a note ${error}`);
-        res.status(500).json({
-            message: "Internal server error",        
-        });
-    }
-}
+  if (note.user.toString() != userId) {
+    res.status(403);
+    throw new Error("Forbidden");
+  }
 
-export const deleteNotes = async (req,res) => {
-    try {
-        const userId = req.user.id;
-        const id = req.params.id;
-        const note = await Note.findById(id);
-        if(userId!=note.user.toHexString()){
-          return res.status(401).json({
-            message:"User Not Authorized"
-          })
-        }
-        const deletedNote = await Note.findByIdAndDelete(id);
-        if(!note){
-            return res.status(404).json({
-                message:"Note not found"
-            })
-        }
-        res.status(200).json({
-            message:"Note deleted successfully",
-            deletedNote:deletedNote
-        })
-        
-    } catch (error) {
-        console.error(`Error in deleting a note ${error}`);
-        res.status(500).json({
-            message: "Internal server error",        
-        });
-    }
-}
+  res.status(200).json({
+    success: true,
+    message: "Note fetched successfully",
+    data: note,
+  });
+});
+
+export const updateNotes = asyncHandler(async (req, res) => {
+  const userId = req.user.id;
+  const id = req.params.id;
+  const updateData = req.body;
+  const note = await Note.findById(id);
+
+  if (!note) {
+    res.status(404);
+    throw new Error("Note not found");
+  }
+
+  if (note.user.toString() != userId) {
+    res.status(401);
+    throw new Error("Not Authorized");
+  }
+
+  const updatedNote = await Note.findByIdAndUpdate(id, updateData, {
+    runValidators: true,
+    returnDocument: "after",
+  });
+
+  if (!updatedNote) {
+    res.status(404);
+    throw new Error("Note not found");
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "Note updated successfully",
+    data: updatedNote,
+  });
+});
+
+export const deleteNotes = asyncHandler(async (req, res) => {
+  const userId = req.user.id;
+  const id = req.params.id;
+  const note = await Note.findById(id);
+
+  if (!note) {
+    res.status(404);
+    throw new Error("Note not found");
+  }
+
+  if (note.user.toString() != userId) {
+    res.status(401);
+    throw new Error("User Not Authorized");
+  }
+
+  const deletedNote = await Note.findByIdAndDelete(id);
+
+  if (!note) {
+    res.status(404);
+    throw new Error("Note not found");
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "Note deleted successfully",
+    data: deletedNote,
+  });
+});
--- projects/NotesApp/backend/src/controllers/users.controllers.js
+++ projects/NotesApp/backend/src/controllers/users.controllers.js
@@ -1,126 +1,100 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import asyncHandler from "../utils/asyncHandler.js";
 
 import User from "../models/users.models.js";
 
-export const userRegister = async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-    if (!username || !email || !password) {
-      return res.status(401).json({
-        message: "Not enough credentials",
-      });
-    }
-    const user = await User.findOne({
-      $or: [{ username: username }, { email: email }],
-    });
-    if (user) {
-      return res.status(409).json({
-        message: "User already exists",
-      });
-    }
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const createdUser = await User.create({
-      username: username,
-      email: email,
-      password: hashedPassword,
-    });
-    const userResponse = createdUser.toObject();
-    delete userResponse.password;
-    res.status(201).json({
-      message: "User created successfully",
-      createdUser: {
-        user: userResponse,
-      },
-    });
-  } catch (error) {
-    console.error(`Error registering user ${error}`);
-    res.status(500).json({
-      message: "Internal server error",
-    });
+export const userRegister = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    res.status(401);
+    throw new Error("Not enough credentials");
   }
-};
 
-export const userLogin = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    if (!email || !password) {
-      return res.status(401).json({
-        message: "provide all credentials",
-      });
-    }
-    const user = await User.findOne({ email }).select("+password");
-    if (!user) {
-      return res.status(404).json({
-        message: "User not registered",
-      });
-    }
-    const isPasswordMatch = await bcrypt.compare(password, user.password);
-    if (!isPasswordMatch) {
-      return res.status(401).json({
-        message: "Invalid credentials",
-      });
-    }
-    const token = jwt.sign(
-      {
-        id: user._id,
-      },
-      process.env.JWT_SECRET,
-      {
-        expiresIn: "7d",
-      },
-    );
+  const user = await User.findOne({
+    $or: [{ username: username }, { email: email }],
+  });
 
-    res.cookie("token",token,{
-        httpOnly:true,
-        secure:process.env.NODE_ENV==="production",
-        sameSite:"strict",
-        maxAge:7*24*60*60*1000
-    })
-    const userResponse = user.toObject();
-    delete userResponse.password;
-
-    res.status(200).json({
-        message:"User login successfully",
-        token:token,
-        user:userResponse
-    })
-  } catch (error) {
-    console.error(`Error user logging in ${error}`);
-    res.status(500).json({
-      message: "Internal server error",
-    });
+  if (user) {
+    return res.status(409);
+    throw new Error("User already exists");
   }
-};
 
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const createdUser = await User.create({
+    username: username,
+    email: email,
+    password: hashedPassword,
+  });
+  const userResponse = createdUser.toObject();
+  delete userResponse.password;
+  res.status(201).json({
+    success: true,
+    message: "User created successfully",
+    data: userResponse,
+  });
+});
 
-export const getUserInfo = async (req,res) => {
-  try {
-        const { id } = req.user;
-        const user = await User.findById(id);
-        res.status(200).json({
-          message:"user fetched successfully",
-          user:user
-        })
-  } catch (error) {
-    console.error(`Error getting user info ${error}`);
-    res.status(500).json({
-      message: "Internal server error",
-    });
+export const userLogin = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(401);
+    throw new Error("provide all credentials");
   }
-}
-
-export const userLogout = async (req,res) => {
-  try {
-         res.clearCookie("token");
-         res.status(200).json({
-          message:"User logged out successfully"
-         })
-    
-  } catch (error) {
-    console.error(`Error getting user logout : ${error}`);
-    res.status(500).json({
-      message: "Internal server error",
-    });
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    res.status(404);
+    throw new Error("User not registered");
   }
-}
+  const isPasswordMatch = await bcrypt.compare(password, user.password);
+  if (!isPasswordMatch) {
+    res.status(401);
+    throw new Error("Invalid credentials");
+  }
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "7d",
+    },
+  );
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+  const userResponse = user.toObject();
+  delete userResponse.password;
+
+  res.status(200).json({
+    success: true,
+    message: "User login successfully",
+    data: {
+      token: token,
+      userResponse: userResponse,
+    },
+  });
+});
+
+export const getUserInfo = asyncHandler(async (req, res) => {
+  const { id } = req.user;
+  const user = await User.findById(id);
+  res.status(200).json({
+    success: true,
+    message: "user fetched successfully",
+    data: user,
+  });
+});
+
+export const userLogout = asyncHandler(async (req, res) => {
+  res.clearCookie("token");
+  res.status(200).json({
+    success: true,
+    message: "User logged out successfully",
+  });
+});
