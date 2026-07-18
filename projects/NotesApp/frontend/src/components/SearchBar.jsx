24b2182f8d3b86ce952e55d0a1ec4ad4fb725a80
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-07-19 01:23:10 +0545

    search bar updated

--- projects/NotesApp/frontend/src/components/SearchBar.jsx
+++ projects/NotesApp/frontend/src/components/SearchBar.jsx
@@ -1,14 +1,14 @@
 export default function SearchBar(){
     return (
-        <div className="flex justify-center items-center gap-4">
+        <form className="flex gap-4">
             <input 
                 type="text"
                 placeholder="search notes" 
-                className="border border-gray-600 p-2 rounded-xl"   
+                className="flex-1 border border-gray-600 p-2 rounded-xl"   
             />
-            <button>
+            <button className="py-2 px-4 border-none bg-blue-500 rounded-lg text-2xl text-white hover:bg-blue-700">
                 Search
             </button>
-        </div>
+        </form>
     )
 }
