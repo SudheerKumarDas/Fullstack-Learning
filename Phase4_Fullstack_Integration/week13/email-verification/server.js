fcf5f86d9ddb05f41cecbc49adab58a1aeab916d
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-07-22 20:25:13 +0545

    server is started on port 3000

---
+++ Phase4_Fullstack_Integration/week13/email-verification/server.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express"
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/",(req,res)=>{
+    res.send("email verification")
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
