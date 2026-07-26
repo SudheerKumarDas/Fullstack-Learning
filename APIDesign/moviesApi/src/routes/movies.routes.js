2d7ced564b5c3a32e6db4e3f9b40df28b09c239c
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-07-26 23:04:57 +0545

    update movie controller

--- APIDesign/moviesApi/src/routes/movies.routes.js
+++ APIDesign/moviesApi/src/routes/movies.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 
-import { createMovies, getAllMovies, getMovieById } from "../controllers/movies.controllers.js";
+import { createMovies, getAllMovies, getMovieById, updateMovies } from "../controllers/movies.controllers.js";
 
 const router = express.Router();
 
 router.post("/",createMovies);
 router.get("/",getAllMovies);
 router.get("/:id",getMovieById);
+router.put("/:id",updateMovies);
 
 export default router;
