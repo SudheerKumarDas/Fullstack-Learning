d399a4fcb55659df81518f627b2282754b7d40fb
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-08-01 22:13:41 +0545

    email verification done

--- projects/EmailVerification/backend/src/routes/user.route.js
+++ projects/EmailVerification/backend/src/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { userRegister, verifyEmail } from "../controllers/user.controller.js";
+import { resendEmailVerification, userRegister, verifyEmail } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.post("/register",userRegister);
 router.get("/verify-email",verifyEmail);
+router.post("/resend-verification",resendEmailVerification);
 
 export default router;
