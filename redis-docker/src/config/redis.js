4cbd9d629532b3a13eae717bbe638ef3e6ee700d
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-08-09 15:13:00 +0545

    redis configured

--- redis-docker/src/config/redis.js
+++ redis-docker/src/config/redis.js
@@ -1,2 +1,15 @@
 import { createClient } from "redis";
 
+const redisClient = await createClient({
+    url:process.env.REDIS_URL
+});
+
+redisClient.on("error",(err)=>{
+    console.error("Redis error :",err);
+})
+
+await redisClient.connect();
+
+console.log("Redis connected");
+
+export default redisClient;
