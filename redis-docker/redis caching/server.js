0ab6dc427e8f80ed83727b6ad8df2db099eb0745
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-08-09 15:57:44 +0545

    count redis which increases sth

--- redis-docker/redis caching/server.js
+++ redis-docker/redis caching/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import "dotenv/config";
 
 import redisClient from "./src/config/redis.js";
+import connectDB from "./src/config/db.js";
 
 const app = express();
 
@@ -10,29 +11,38 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
+  res.json({
+    message: "Redis learning API",
+  });
+});
+
+app.get("/redis-test", async (req, res) => {
+  await redisClient.set("name", "Alex");
+  const name = await redisClient.get("name");
+  res.json({
+    name,
+  });
+});
+
+app.get("/test-expiration", async (req, res) => {
+  await redisClient.set("temporary", "Hello Redis", {
+    EX: 30,
+  });
+  res.json({
+    message: "Key will expire in 30 seconds",
+  });
+});
+
+app.get("/visit",async(req,res)=>{
+    const count = await redisClient.incr("visits");
     res.json({
-        message:"Redis learning API"
+        visits:count
     })
 })
 
-app.get("/redis-test",async(req,res)=>{
-    await redisClient.set("name","Alex");
-    const name = await redisClient.get("name");
-    res.json({
-        name
-    })
-})
-
-app.get("/test-expiration",async(req,res)=>{
-    await redisClient.set("temporary","Hello Redis",{
-        EX:30
-    });
-    res.json({
-        message:"Key will expire in 30 seconds"
-    })
-})
-
-app.listen(PORT,()=>{
+connectDB().then(() => {
+  app.listen(PORT, () => {
     console.log(`App is listening on port http://localhost:${PORT}`);
-})
+  });
+});
