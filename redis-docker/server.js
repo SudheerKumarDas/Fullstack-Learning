699297b820704f3299a4bc41ab287ac2e0a92157
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-08-09 15:22:24 +0545

    redis and express app is running

--- redis-docker/server.js
+++ redis-docker/server.js
@@ -1,1 +1,21 @@
 import express from "express";
+
+import "dotenv/config";
+
+import redisClient from "./src/config/redis.js";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.json({
+        message:"Redis learning API"
+    })
+})
+
+app.listen(PORT,()=>{
+    console.log(`App is listening on port http://localhost:${PORT}`);
+})
