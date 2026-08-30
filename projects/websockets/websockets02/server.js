ac35020dfb6dd735d785297871a81926707b385c
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-08-30 14:36:48 +0545

    message broadcasts to all the clients

--- projects/websockets/websockets02/server.js
+++ projects/websockets/websockets02/server.js
@@ -3,14 +3,24 @@ const WebSocket = require("ws");
 const wss = new WebSocket.Server({ port:8080 });
 
 wss.on("connection",(ws)=>{
-    console.log("A new client connected!");
+    console.log("A new client connected : ", Math.floor(Math.random()*100)+1);
 
-    ws.on("message",(message)=>{
-        console.log("Client : ",message.toString());
-        ws.send("Server : Hello client, How are you ?");
+    //only to the one client and server
+    // ws.on("message",(message)=>{
+    //     console.log("Client : ",message.toString());
+    //     ws.send("Server : Hello client, How are you ?");
+    // })
+
+    //ws.send("Hello From Server");
+
+    //broadcast to all the clients that are connected to websocket server including the one that sends
+    ws.on("message",(data)=>{
+        wss.clients.forEach((client)=>{
+            if(client.readyState===client.OPEN){
+                client.send(data.toString());
+            }
+        })
     })
-
-    ws.send("Hello From Server");
 
     // ws.close("close",()=>{
     //     console.log("Client disconnected...")
